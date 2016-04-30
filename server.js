f01f64766df596e0c4803adab38d85efe6ec8114
Sumit Kumar <sumit.kumar@vvdntech.in>
2016-05-01 00:37:03 +0530

    added bootstrap to make page beautiful

--- server.js
+++ server.js
@@ -6,6 +6,30 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 app.use(express.static(__dirname + '/public'));
 var clientInfo = {};
+
+// Send surrent users to provided sockets
+
+function sendCurrentUsers(socket){
+	var info = clientInfo[socket.id];
+	var users = [];
+	if(typeof info === 'undefined')
+		return;
+	Object.keys(clientInfo).forEach(function(socketId){
+		var userInfo = clientInfo[socketId];
+		
+			if(info.room === userInfo.room){
+				users.push(userInfo.name);
+			}
+			
+			
+	});
+	socket.emit('message' , {
+				name : 'System',
+				text : 'Current users: ' + users.join(', '),
+				timestamp : moment.valueOf()
+			})
+
+}
 io.on('connection' , function(socket){
 
 console.log("User connected via socket io!!!!");
@@ -19,6 +43,7 @@ socket.on('disconnect' , function(){
 			timestamp : moment.valueOf()
 		});
 	}
+	delete clientInfo[socket.id];
 });
 
 socket.on('joinRoom' , function(req){
@@ -34,8 +59,14 @@ socket.on('joinRoom' , function(req){
 socket.on('message' , function(message){
 	console.log('Message Recived');
 	console.log(message.text);
-	message.timestamp = moment.valueOf();
-	io.to(clientInfo[socket.id].room).emit('message',message);
+	if(message.text == '@currentUsers'){
+
+		sendCurrentUsers(socket);
+	}
+	else {
+		message.timestamp = moment.valueOf();
+		io.to(clientInfo[socket.id].room).emit('message',message);
+	}
 });
 
 
